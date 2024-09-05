919a135601e71539b98901dc437b1c799d9c591b
Andestid <andestid@outlook.es>
2024-09-05 11:19:56 -0500

    despliegue

--- db.js
+++ db.js
@@ -1,19 +1,29 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-const mysql = require('mysql');
+const mysql = require('mysql2'); // Cambia esto de 'mysql' a 'mysql2'
+
+
 let connection;
 
 try {
     connection = mysql.createConnection({
         host: process.env.MYSQLHOST,
         user: process.env.MYSQLUSER,
-        password: process.env.MYSQL_ROOT_PASSWORD,
+        password: process.env.MYSQLPASSWORD,
         database: process.env.MYSQLDATABASE,
         port: process.env.MYSQLPORT
     });
+
+    connection.connect((err) => {
+        if (err) {
+            console.error("Error al conectar con la base de datos:", err.message);
+            return;
+        }
+        console.log("Conectado a la base de datos.");
+    });
 } catch (error) {
-    console.log("Error al conectar con la base de datos");
+    console.error("Error al configurar la conexión a la base de datos:", error.message);
 }
 
-module.exports = {connection};
+module.exports = { connection };
